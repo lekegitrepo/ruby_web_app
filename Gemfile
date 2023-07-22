a9b03cbd17bc890a522ebb1890b926b3a85a34b9
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-07-22 08:08:08 +0100

    Install erb

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
+# it allows ruby code to be embeded in view https://github.com/ruby/erb
+gem 'erb', '~> 4.0', '>= 4.0.2'
+
 gem "puma", "~> 6.3"
 
 gem "rack", "~> 3.0"
--- app.rb
+++ app.rb
@@ -1,11 +1,26 @@
+require 'erb'
+
 class App
   def call(env)
     headers = {
       'Content-Type' => 'text/html',
     }
 
-    response = File.read 'views/index.html'
+    title = get_title(env) || 'Ruby on Rails Tutorial'
+    erb = ERB.new(html_template)
+  
+    response =  erb.result(binding) # File.read 'views/index.html'
 
     [200, headers, [response]]
+  end
+
+  def html_template
+    File.read 'views/index.html.erb'
+  end
+
+  def get_title(env)
+    query = env['QUERY_STRING'] # "title=ruby"
+    values = query.split('=')   # ["title", "ruby"]
+    values[1]                   # ruby
   end
 end
