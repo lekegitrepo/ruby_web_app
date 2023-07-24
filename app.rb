8f04f672a4b08281f81b2416b64f789748efc242
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-07-24 07:56:03 +0100

    Create router.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,28 @@
 require 'erb'
+require_relative './router'
 
 class App
+  attr_reader :router
+
+  def initialize
+    @router = Router.new
+    
+    router.get('/') { 'Home page '}
+    router.get('/posts') { 'Posts/Articles page '}
+    router.get('/posts/1') { 'First post/article page '}
+  end
+
   def call(env)
     headers = {
       'Content-Type' => 'text/html',
     }
 
-    title = get_title(env) || 'Ruby on Rails Tutorial'
-    erb = ERB.new(html_template)
+    # title = get_title(env) || 'Ruby on Rails Tutorial'
+    # erb = ERB.new(html_template)
   
-    response =  erb.result(binding) # File.read 'views/index.html'
+    # response =  erb.result(binding) # File.read 'views/index.html'
+
+    response = router.build_response(env['REQUEST_PATH'])
 
     [200, headers, [response]]
   end
